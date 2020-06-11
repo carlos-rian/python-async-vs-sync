daa998dd42f6c53a6d3a34392aa711ee402d00c0
carlosrian300 <carlos.silva@empiricus.com.br>
2020-06-11 15:18:38 -0300

    porcentagem -> eficiencia

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ from time import time
 init = time()
 print(15 * "+=", "Iniciando Test Sync", 15 * "=+")
 test_sync.main()
-print(10 * "+=", f"Test Sync Finalizado em {time()-init:.2f} Seg", 10 * "=+")
+tempo_sync = time()-init
+print(10 * "+=", f"Test Sync Finalizado em {tempo_sync:.2f} Seg", 10 * "=+")
 
 
 
 init = time()
 print(15 * "+=", "Iniciando Test Async", 15 * "=+")
 test_async.main()
-print(10 * "+=", f"Test Async Finalizado em {time()-init:.2f} Seg", 10 * "=+")
+tempo_async = time()-init
+print(12 * "+=", f"Test Async Finalizado em {tempo_async:.2f} Seg", 12 * "=+")
 
+
+porcentagem = 1 - (tempo_async/tempo_sync)
+print(f"Async foi {porcentagem:.2f}% mais eficiente que Sync")
